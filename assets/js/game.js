aed39850fe89b3a673423f3ebb92373cb88c0815
Amir Shariat <amirshariat1987@gmail.com>
2021-09-26 19:55:15 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,11 +2,10 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-// You can also log multiple values at once like this
-// console.log(playerName, playerAttack, playerHealth);
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+// fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while(playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
@@ -51,18 +50,21 @@ var fight = function(enemyName) {
     };
   };
 };
+// fight each enemy-robot by looping over them and fighting them one at a time
 for(var i = 0; i < enemyNames.length; i++) {
+  // if player is still alive, keep fighting
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    // pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+    // reset enemyHealth before starting new fight
+    enemyHealth = 50;
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+    // if player isn't alive, stop the game
   } else {
-    window.alert("You have lost your robot in battle! Game Over!");
-    break;
+  window.alert("You have lost your robot in battle! Game Over!");
+  break;
   };
-  // pick new enemy to fight based on the index of the enemyNames array
-  var pickedEnemyName = enemyNames[i];
-  // reset enemyHealth before starting new fight
-  enemyHealth = 50;
-  // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-  fight(pickedEnemyName);
 };
